c646d95a58c917c8efd7e77378e103cecc156947
olim7t <omichallat+github@gmail.com>
2010-06-30 22:57:40 +0200

    Initial import.

---
+++ src/main/scala/ScalariformPlugin.scala
@@ -0,0 +1,46 @@
+package com.github.olim7t.sbtscalariform
+
+import sbt._
+import scala.io.Source
+import sbt.Fork.ForkScala
+import java.io.File
+
+trait ScalariformPlugin extends BasicScalaProject with SourceTasks {
+	import ScalariformPlugin._
+
+	val sfConfig = config("sfConfig") hide
+	val scalariform = "com.github.mdr" % "scalariform.core" % "0.0.4" % "sfConfig" from "http://scalariform.googlecode.com/svn/trunk/update-site/plugins/scalariform.core_0.0.4.201006281921.jar"
+	def scalariformClasspath = descendents(configurationPath(sfConfig), "*.jar").absString
+
+	def formatterScalaJars = {
+		val si = getScalaInstance(ScalariformScalaVersion)
+		si.libraryJar :: si.compilerJar :: Nil
+	}
+
+	lazy val formatSources = formatSourcesAction
+	lazy val formatTests = formatTestsAction
+
+	def sourceTimestamp = "sources.lastFormatted"
+	def testTimestamp = "tests.lastFormatted"
+
+	def formatSourcesAction = forAllSourcesTask(sourceTimestamp from mainSources)(runFormatter) describedAs("Format main Scala sources")
+	def formatTestsAction = forAllSourcesTask(testTimestamp from testSources)(runFormatter) describedAs("Format test Scala sources")
+
+	override def compileAction = super.compileAction dependsOn(formatSources)
+	override def testCompileAction = super.testCompileAction dependsOn(formatTests)
+
+	private def runFormatter(sources: Iterable[Path]): Option[String] = {
+		val forkFormatter = new ForkScala(ScalariformMainClass)
+		for (source <- sources) {
+			log.debug("Formatting " + source)
+			forkFormatter(None, Seq("-cp", scalariformClasspath) , formatterScalaJars, Seq("-i", source.absolutePath), log) 
+		}
+		None
+	}
+}
+object ScalariformPlugin {
+	/** The version of Scala used to run Scalariform.*/
+	val ScalariformScalaVersion = "2.8.0.RC6"
+
+	val ScalariformMainClass = "scalariform.commandline.Main"
+}
---
+++ src/main/scala/SourceTask.scala
@@ -0,0 +1,57 @@
+package com.github.olim7t.sbtscalariform
+
+import sbt._
+import java.io.File
+
+trait TimestampSources extends NotNull {
+	def timestamp: File
+	def sources: Iterable[Path]
+}
+trait TimestampWrapper extends NotNull {
+	def from(sources: => Iterable[Path]): TimestampSources =
+		from(Path.lazyPathFinder(sources))
+	def from(sources: PathFinder): TimestampSources
+}
+
+/** Provides methods to define tasks with basic conditional execution based on the sources of
+* the task and a timestamp file in the output directory.
+* When the task is run, we want to process sources newer than the timestamp; then the timestamp
+* is touched.
+*/
+trait SourceTasks extends Project {
+	implicit def wrapTimestamp(name: String): TimestampWrapper =
+		SourceTasks.wrapTimestamp(outputPath, name)
+
+	/** Runs an individual action on each modified source. */
+	def forEachSourceTask(label: String, files: TimestampSources)(action: Path => Option[String]): Task =
+		task { SourceTasks.processEach(label, files, log)(action) }
+	def forEachSourceTask(files: TimestampSources)(action: Path => Option[String]): Task = forEachSourceTask("", files)(action)
+
+	/** Runs a global action that takes the list of modified sources. */
+	def forAllSourcesTask(label: String, files: TimestampSources)(action: Iterable[Path] => Option[String]): Task =
+		task { SourceTasks.processAll(label, files, log)(action) }
+	def forAllSourcesTask(files: TimestampSources)(action: Iterable[Path] => Option[String]): Task =
+		forAllSourcesTask("", files)(action)
+}
+object SourceTasks {
+	implicit def wrapTimestamp(basePath: Path, name: String): TimestampWrapper = new TimestampWrapper {
+		def from(sourceFinder: PathFinder) = new TimestampSources {
+			def timestamp = basePath / name asFile
+			def sources = sourceFinder.get
+		}
+	}
+	def processEach(label: String, files: TimestampSources, log: Logger)(action: Path => Option[String]): Option[String] = {
+		import files._
+		val fileErrors = sources.filter(_.lastModified > timestamp.lastModified).flatMap(action(_))
+		val touchError = FileUtilities.touch(timestamp, log)
+		val errors = fileErrors ++ touchError
+		if (errors.isEmpty) None else Some(errors mkString "\n")
+	}
+	def processAll(label: String, files: TimestampSources, log: Logger)(globalAction: Iterable[Path] => Option[String]): Option[String] = {
+		import files._
+		val actionError = globalAction(sources.filter(_.lastModified > timestamp.lastModified))
+		val touchError = FileUtilities.touch(timestamp, log)
+		val errors = actionError ++ touchError
+		if (errors.isEmpty) None else Some(errors mkString "\n")
+	}
+}
